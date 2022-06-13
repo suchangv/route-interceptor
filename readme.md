15daa028b8fa26a5e95f9462d2abef3302ad4e27
suchangv <suchangv@gmail.com>
2022-06-14 03:47:47 +0800

    complete a,window.open,history,hash

---
+++ readme.md
@@ -0,0 +1,9 @@
+// const interceptor = RouteIntercept.create({
+//   way: ["location", "a", "window.open", "history", "hash"],
+//   intercept: async (from) => {
+//     return from.location.replace("a", "b");
+//   },
+// });
+
+// interceptor.start();
+// interceptor.stop();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,155 @@
-type InterceptCallback = (from: string) => Promise<string>;
+export type Way = "a" | "window.open" | "history" | "hash" | "location";
 
-function intercept(callback: InterceptCallback) {
-  
+export type Intercept = (url: string) => string | false;
+
+export type InterceptorInstance = {
+  start: () => void;
+};
+
+const originWindowOpen = window.open;
+
+let isStarted = false;
+
+export function create({
+  way,
+  intercept,
+}: {
+  way: Way[];
+  intercept: Intercept;
+}): InterceptorInstance {
+  const start = () => {
+    if (isStarted) {
+      return;
+    }
+    isStarted = true;
+
+    if (way.includes("a")) {
+      _a(intercept);
+    }
+    if (way.includes("window.open")) {
+      _windowOpen(intercept);
+    }
+    if (way.includes("history")) {
+      _history(intercept);
+    }
+    if (way.includes("hash")) {
+      _hash(intercept);
+    }
+    if (way.includes("location")) {
+    }
+  };
+
+  return {
+    start,
+  };
+}
+
+/**
+ * intercept all Anchor tag
+ */
+function _a(intercept: Intercept) {
+  // Mouse Event path polyfill
+  if (!("path" in Event.prototype))
+    Object.defineProperty(Event.prototype, "path", {
+      get: function () {
+        const path = [];
+        let currentElem = this.target;
+        while (currentElem) {
+          path.push(currentElem);
+          currentElem = currentElem.parentElement;
+        }
+        if (path.indexOf(window) === -1 && path.indexOf(document) === -1) path.push(document);
+        if (path.indexOf(window) === -1) path.push(window);
+        return path;
+      },
+    });
+
+  document.addEventListener("click", function (event) {
+    const path: HTMLElement[] | undefined = (event as any).path;
+    if (path === undefined) return;
+
+    if (event.defaultPrevented) {
+      return;
+    }
+
+    for (const item of path) {
+      if (item instanceof HTMLAnchorElement) {
+        const to = intercept(item.href);
+        if (to === false) {
+          return;
+        }
+
+        event.preventDefault();
+        return originWindowOpen.call(window, to, item.target);
+      }
+    }
+  });
+}
+
+/**
+ * intercept window.open
+ */
+function _windowOpen(intercept: Intercept) {
+  window.open = (url?: string | URL, target?: string, features?: string): WindowProxy | null => {
+    const path = url?.toString();
+    if (path === undefined) {
+      return null;
+    }
+
+    const to = intercept(path);
+    if (to === false) {
+      return null;
+    }
+
+    return originWindowOpen(to, target, features);
+  };
+}
+
+/**
+ * intercept history pushState & replaceState
+ */
+function _history(intercept: Intercept) {
+  ["pushState", "replaceState"].forEach((funcName) => {
+    const funcNameWithType = funcName as "pushState" | "replaceState";
+
+    const originFunc = history[funcNameWithType];
+
+    history[funcNameWithType] = (data: any, unused: string, url?: string | URL | null) => {
+      if (url === undefined || url === null) {
+        return;
+      }
+
+      console.log(url);
+
+      const to = intercept(url.toString());
+      if (to === false) {
+        return;
+      }
+
+      return originFunc.call(history, data, unused, to);
+    };
+  });
+}
+
+let _nextHash = "";
+function _hash(intercept: Intercept) {
+  window.addEventListener("hashchange", function (event) {
+    const url = new URL(event.newURL);
+    const { hash } = url;
+
+    if (hash === _nextHash) {
+      _nextHash = "";
+      return;
+    }
+
+    const to = intercept(hash.slice(1));
+
+    if (to === false) {
+      return;
+    }
+
+    _nextHash = `#${to}`;
+
+    this.location.hash = _nextHash;
+  });
 }
