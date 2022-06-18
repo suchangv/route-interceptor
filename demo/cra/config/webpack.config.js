e31f9a594fed39185e2f508d884be2d2dd76bc09
suchangv <suchangv@gmail.com>
2022-06-19 04:29:09 +0800

    demo/cra add babel plugin

--- demo/cra/config/webpack.config.js
+++ demo/cra/config/webpack.config.js
@@ -424,6 +424,7 @@ module.exports = function (webpackEnv) {
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
                     require.resolve('react-refresh/babel'),
+                  require.resolve('@suchangv/route-interceptor/plugins/babel'),
                 ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
